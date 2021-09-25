c804103bccb8c1ef0307caeb2ddd06c569c46f74
gonzalezsebastian <gonzalezes@uninorte.edu.co>
2021-09-25 15:38:27 -0500

    Changes tables style Padre and Hijos

---
+++ README.md
@@ -0,0 +1,9 @@
+# databaseCRUD
+Simple CRUD using Flask and Oracle XE
+This project was made for a CS Databases course.
+
+# Instructions
+
+1. Install all requirements using `pip install -r requirements.txt` 
+2. In your terminal: `flask run`
+3. The Webapp should now be running in localhost
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 import cx_Oracle
-from DATA import db_user, db_password, db_dsn, db_encoding 
+from DATA import db_user, db_password, db_dsn, db_encoding
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,6 +23,82 @@ def hijo():
     
     return render_template("hijo.html", headings=headings, data=data)
 
+@app.route('/create-hijo', methods=['POST'])
+def create_hijo():
+    
+    with cx_Oracle.connect(
+            user=db_user,
+            password=db_password,
+            dsn=db_dsn,
+            encoding=db_encoding
+        ) as conn:
+        
+        id_hijo = request.form['id_hijo']
+        nombre_hijo = request.form['nombre_hijo']
+        id_padre = request.form['id_padre']
+        cursor = conn.cursor()
+        
+        try:
+            cursor.execute(f"INSERT INTO HIJO (ID, NOM, HIJODE) VALUES ({id_hijo}, '{nombre_hijo}', {id_padre})")
+            conn.commit()
+        except cx_Oracle.DatabaseError as e:
+            pass
+        finally:
+            return redirect(url_for('hijo'))
+        
+@app.route('/deleteHijo/<id>')
+def delete_hijo(id):
+    with cx_Oracle.connect(
+            user=db_user,
+            password=db_password,
+            dsn=db_dsn,
+            encoding=db_encoding
+        ) as conn:
+        
+        cursor = conn.cursor()
+        
+        try:
+            cursor.execute(f"DELETE FROM HIJO WHERE ID={int(id)}")
+            conn.commit()
+        except cx_Oracle.DatabaseError as e:
+            pass
+        finally:
+            return redirect(url_for('hijo'))
+
+
+
 @app.route('/padre.html')
 def padre():
-    return render_template("padre.html")
+    
+    with cx_Oracle.connect(
+            user=db_user,
+            password=db_password,
+            dsn=db_dsn,
+            encoding=db_encoding
+        ) as conn:
+        cursor = conn.cursor()
+        cursor.execute("select * from padre")
+        data = cursor.fetchall()
+        headings = [row[0] for row in cursor.description]
+    
+    return render_template("padre.html", headings=headings, data=data)
+
+@app.route('/consulta1.html')
+def consulta1():
+    
+    return render_template("consulta1.html")
+
+@app.route('/consulta2.html')
+def consulta2():
+    
+    return render_template("consulta2.html")
+
+@app.route('/consulta3.html')
+def consulta3():
+    
+    return render_template("consulta3.html")
+
+@app.route('/consulta4.html')
+def consulta4():
+    
+    return render_template("consulta4.html")
